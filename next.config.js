792c76e585328d3a151957fc6a931356d3e99275
Thomas Bouder <tbouder@protonmail.com>
2021-08-09 11:51:33 +0200

    📝 Import not imported env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /******************************************************************************
 **	@Author:				Thomas Bouder <Tbouder>
 **	@Email:					Tbouder@protonmail.com
-**	@Date:					Monday August 24th 2020
+**	@Date:					Sunday June 13th 2021
 **	@Filename:				next.config.js
 ******************************************************************************/
 
@@ -12,14 +12,16 @@ module.exports = ({
 		new Dotenv()
 	],
 	env: {
+		PROJECT_LOGO: process.env.PROJECT_LOGO,
 		PROJECT_TITLE: process.env.PROJECT_TITLE,
 		TITLE: process.env.TITLE,
 		DESCRIPTION: process.env.DESCRIPTION,
 		BUTTON: process.env.BUTTON,
 		IMAGE: process.env.IMAGE,
+		APP: process.env.APP,
 		REPOSITORY: process.env.REPOSITORY,
 		CONTRACT: process.env.CONTRACT,
-		APP: process.env.APP,
+		CREDITS: process.env.CREDITS,
 	},
 	optimization: {
 		minimize: true,
